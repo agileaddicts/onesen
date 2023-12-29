1093a3b9231396b32b9aea62c35355f4d629e0f7
Sebastian Göttschkes <sebastian@agileaddicts.com>
2023-12-29 10:30:26 +0100

    chore: add test for content update

--- test/onesen_web/live/notebook_live/show_test.exs
+++ test/onesen_web/live/notebook_live/show_test.exs
@@ -1,21 +1,31 @@
 defmodule OnesenWeb.NotebookLiveTest do
-  alias Onesen.Models.Notebook
   use OnesenWeb.ConnCase
 
   import Phoenix.LiveViewTest
 
   alias Onesen.Models.Notebook
+  alias Onesen.Models.Page
 
-  defp create_notebook(_) do
+  setup do
     notebook = Notebook.create!()
     %{notebook: notebook}
   end
 
-  setup [:create_notebook]
-
   test "displays notebook", %{conn: conn, notebook: notebook} do
-    {:ok, _show_live, html} = live(conn, ~p"/n/#{notebook.identifier}")
+    {:ok, _view, html} = live(conn, ~p"/n/#{notebook.identifier}")
 
     assert html =~ "Your notebook"
+    assert html =~ Date.utc_today() |> to_string()
+  end
+
+  test "writes content in notebook", %{conn: conn, notebook: notebook} do
+    {:ok, view, _html} = live(conn, ~p"/n/#{notebook.identifier}")
+
+    assert view
+           |> element("form")
+           |> render_change(%{content: "Lorem ipsum"}) =~ "Lorem ipsum"
+
+    loaded_page = Page.get_today(notebook)
+    assert loaded_page.content == "Lorem ipsum"
   end
 end
