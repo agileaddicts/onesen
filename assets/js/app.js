defb83dde41603554d1f86ee51310cf78535ee88
Sebastian Göttschkes <sebastian@agileaddicts.com>
2024-02-05 16:55:23 +0100

    chore: add alpinejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,7 @@
 //     import "some-package"
 //
 
+import Alpine from 'alpinejs'
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
@@ -39,3 +40,6 @@ liveSocket.connect()
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 
+// Alpine
+window.Alpine = Alpine
+Alpine.start()
