1b69ab0ec0dbb33fab0e5c4b1a903c5848894df6
Sebastian Göttschkes <sebastian@agileaddicts.com>
2024-04-29 11:44:56 +0200

    chore: update dependencies

--- config/config.exs
+++ config/config.exs
@@ -32,7 +32,7 @@ config :onesen, OnesenWeb.Endpoint,
 config :onesen, Onesen.Mailer, adapter: Swoosh.Adapters.Local
 
 config :bun,
-  version: "1.1.4",
+  version: "1.1.6",
   default: [
     args:
       ~w(build js/app.js --outdir=../priv/static/assets --external /fonts/* --external /images/*),
